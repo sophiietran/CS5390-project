b2607420598eefc648353ae2ee3bb65f8ad024b0
Sophie Tran <sophiie.tran@gmail.com>
2025-04-18 12:30:21 -0500

    Implement multiple clients and session logging

--- src/TCPClient.java
+++ src/TCPClient.java
@@ -2,15 +2,16 @@ import java.io.*;
 import java.net.*;
 import java.util.Random;
 import java.util.Scanner;
-import java.nio.charset.StandardCharsets;
+
 
 
 class TCPClient {
-    static final int NUM_EQUATIONS_TO_SEND = 10;
+    static final int NUM_EQUATIONS_TO_SEND = 5; // i changed it to 5 so i can see the flow
     static final char[] OPERATORS = {'+', '-', '*', '/', '%'};
 
     public static void main(String[] argv) throws Exception {
         Random random = new Random();
+        Scanner scanner = new Scanner(System.in); // to get user name
 
         // Connect to server and send equation messages
         try (Socket clientSocket = new Socket("127.0.0.1", TCPServer.SERVER_PORT)) {
@@ -19,11 +20,16 @@ class TCPClient {
             System.out.println("Client connected to server using socket " + clientSocket.getLocalPort());
 
             // Tell the server our name, wait for reply
-            outToServer.writeObject(new Message(Message.MessageType.HELLO, "Client1"));
+            System.out.print("Enter username: ");
+            String username = scanner.nextLine();
+            outToServer.writeObject(new Message(Message.MessageType.HELLO, username));
             String ack = inFromServer.readLine();
             System.out.println("Server acknowledged our name: " + ack + "\n");
 
             for (int i = 0; i < NUM_EQUATIONS_TO_SEND; i++) {
+                // randomly wait a little bit
+                int waitMillis = 1000 + random.nextInt(4000); // 1-5 seconds
+                Thread.sleep(waitMillis);
                 // Randomly generate equation
                 Message equation = new Message( // TODO: for now only use positive values so python spits out the same answer as java for any modulo operation
                         //random.nextDouble() * 20 - 10,
@@ -44,7 +50,7 @@ class TCPClient {
                 String solution = inFromServer.readLine();
                 System.out.println("Received solution: " + solution);
 
-                // Run equation through Python for now to verify correctness
+                /* // Run equation through Python for now to verify correctness
                 Process p = Runtime.getRuntime().exec("C:/Users/User/AppData/Local/Programs/Python/Python313/python.exe -c print(f'{" + equation.toString2() + "}')");
                 Scanner s = new Scanner(p.getInputStream(), StandardCharsets.UTF_8);
                 String expected = s.nextLine();
@@ -56,7 +62,7 @@ class TCPClient {
                     System.out.println("Server response does not match expected solution. Terminating.");
                     outToServer.writeObject(new Message(Message.MessageType.QUIT));
                     return;
-                }
+                } */
             }
 
             System.out.println("Done. Terminating connection to server.");
--- src/TCPServer.java
+++ src/TCPServer.java
@@ -1,27 +1,48 @@
 import java.io.*;
 import java.net.*;
+import java.time.Duration;
+import java.time.LocalDateTime;
+import java.util.concurrent.*;
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.Map;
 
 
 // TODO: make something to keep track of connections and their details. each client will also need their own socket.
 
 class TCPServer {
     public static final int SERVER_PORT = 9001;
+    private static final Map<String, ClientSession> clientLog = new ConcurrentHashMap<>(); // logs client sessions
+    private static final BlockingQueue<CalcRequest> requestQueue = new LinkedBlockingQueue<>(); // queue to store incoming CALC requests
 
     public static void main(String[] argv) throws Exception {
+
         // Await and accept connection
         try (ServerSocket welcomeSocket = new ServerSocket(SERVER_PORT)) {
             System.out.println("Server listening for connections on port " + welcomeSocket.getLocalPort());
-            Socket connectionSocket = welcomeSocket.accept();
-            System.out.println("Connection established.\n");
-            ObjectInputStream inFromClient = new ObjectInputStream(connectionSocket.getInputStream());
+            
+
+            // start a worker thread to watch request queue and process calculations
+            new Thread(new CalcWorker()).start();
+
+            // thread to accept clients continuously
+            while(true){
+
+                Socket connectionSocket = welcomeSocket.accept();
+                // System.out.println("Connection established.");
+                new Thread(new ClientHandler(connectionSocket)).start(); //starts new thread for each client connected
+            }
+
+            /* ObjectInputStream inFromClient = new ObjectInputStream(connectionSocket.getInputStream());
             DataOutputStream outToClient = new DataOutputStream(connectionSocket.getOutputStream());
 
             // First message client will send is to tell us their name. Echo it back to them as acknowledgment.
             Message hello = (Message) inFromClient.readObject();
             outToClient.writeBytes(hello.getClientName() + '\n');
+            System.out.println("User: " + hello.getClientName() + " has connected");
             outToClient.flush();
+            */ // moved to clientHandler class
 
-            while (true) {
+            /*  while (true) {
                 // Read message from client
                 Message equation = (Message) inFromClient.readObject();
                 if (equation.getMessageType() == Message.MessageType.QUIT) {
@@ -39,13 +60,15 @@ class TCPServer {
                     outToClient.writeBytes(solution + '\n');
                     outToClient.flush();
                 }
-            }
+            } */ // moved to calcWorker class
+
         } catch (EOFException e) {
             // Socket closed normally
         } catch (Exception e) {
             System.out.println("Exception caught in main: " + e.getMessage());
         }
     }
+
 
     public static Double calculate(Message eq) {
         // TODO: try-catch and throw exception for invalid equation
@@ -76,4 +99,133 @@ class TCPServer {
     private static boolean operatorIsPriority(Character op) {
         return op == '*' || op == '/' || op == '%';
     }
+
+
+
+    // Calculates session duration
+    static class ClientSession {
+        String name;
+        LocalDateTime connectTime;
+        BufferedWriter output;
+        
+        public ClientSession(String name, LocalDateTime connectTime, BufferedWriter out){
+            this.name = name;
+            this.connectTime = connectTime;
+            this.output = out;
+        }
+
+        public void send(String response){
+            try{
+                output.write(response + "\n");
+                output.flush();
+            }catch(IOException e){
+                System.err.println("Response failed to send to client: " + e.getMessage());
+            }
+        }
+    }
+
+    // Represents a math request from client
+    static class CalcRequest{
+        String name;
+        Message equation;
+
+        CalcRequest(String name, Message equation){
+            this.name = name;
+            this.equation = equation;
+        }
+    }
+
+    // handles all math requests in FIFO order
+    static class CalcWorker implements Runnable{
+        public void run(){
+            while(true){
+                try{
+                    CalcRequest request = requestQueue.take(); // blocking queue will wait until there is something in the queue -> do we need to put in a name(?)
+                    Message eq = request.equation;
+                    String solution;
+
+                    if(isValidOperation(eq.operator1, eq.operand2, eq.operator2, eq.operand3))
+                        solution = String.format("%.4f", calculate(eq));
+                    else
+                        solution = "Invalid equation";
+
+                    // gets proper client to send to
+                    ClientSession session = clientLog.get((request.name));
+                    if(session != null){ //if they havent disconnected
+                        session.send(solution);
+                        System.out.println("Processed for " + request.name + ": " + eq + " = " + solution);
+                    }
+                }catch(Exception e){
+                    System.err.println("Error in CalcWorker(): " + e.getMessage());
+                }
+            }
+        }
+    }
+
+    static class ClientHandler implements Runnable {
+    
+        private final Socket socket;
+        private String clientName;
+        private LocalDateTime connectTime;
+        private AtomicBoolean connected = new AtomicBoolean(true); //atomic booleans are thread-safe booleans (this is new to me)
+    
+        public ClientHandler(Socket socket){
+            this.socket = socket;
+        }
+    
+        public void run(){
+            try{
+                ObjectInputStream inFromClient = new ObjectInputStream(socket.getInputStream());
+                BufferedWriter outToClient = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
+    
+                // First message client will send is to tell us their name. Echo it back to them as acknowledgment.
+                Message hello = (Message) inFromClient.readObject();
+                clientName = hello.getClientName();
+                connectTime = LocalDateTime.now();
+
+                outToClient.write(clientName + '\n');
+                outToClient.flush();
+
+                // log connection
+                clientLog.put(clientName, new ClientSession(clientName, connectTime, outToClient));
+                System.out.println("User: " + clientName + " has connected at " + connectTime);
+
+                // handle incoming messages
+                while (connected.get()) {
+                    Message equation = (Message) inFromClient.readObject();
+
+                    if (equation.getMessageType() == Message.MessageType.QUIT) {
+                        // System.out.println("Client has terminated connection.");
+                        disconnect();
+                        break;
+                    } else if (equation.getMessageType() == Message.MessageType.CALC) {
+                        System.out.println("Received equation from" + clientName + ": " + equation);
+                        requestQueue.put(new CalcRequest(clientName, equation));
+                    } else{
+                        System.out.println("Unknown message type from " + clientName);
+                    }
+                        
+                }
+            }catch (Exception e) {
+                System.out.println("Error with client " + clientName + ": " + e.getMessage());
+            }
+        }
+        public void disconnect(){
+            connected.set(false);
+            LocalDateTime disconnectTime = LocalDateTime.now();
+            Duration duration = Duration.between(connectTime, disconnectTime);
+
+            clientLog.remove(clientName);
+            System.out.println(clientName + " has terminated connection after " + duration.toSeconds() + " seconds.");
+
+            try {
+                socket.close();
+            } catch (Exception e) {
+                
+            }
+            
+        }
+
+    }
 }
+       
